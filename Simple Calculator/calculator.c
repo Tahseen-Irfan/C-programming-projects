47781f5a212f6783e0c4dd8e8c4387dc3c1e3d1a
Tahseen Irfan <thaseenirfan.devhub@gmail.com>
2024-10-20 21:03:23 +0600

    Add files via upload

---
+++ Simple Calculator/calculator.c
@@ -0,0 +1,86 @@
+#include<stdio.h>
+
+float add( float num1 , float num2 );
+float sub( float num1 , float num2 );
+float mul( float num1 , float num2 );
+float div( float num1 , float num2 );
+
+int main()
+{
+    int choice;
+    float num1 , num2 , result;
+
+    printf("1. addition\n");
+    printf("1. subtraction\n");
+    printf("1. multiplication\n");
+    printf("1. division\n");
+
+
+    printf("enter your choice(1/2/3/4) : \n");
+    scanf("%d",&choice);
+
+    if(choice >= 1 && choice <= 4)
+    {
+        printf("Enter The Num1:\n");
+        scanf("%f",&num1);
+
+        printf("Enter The Num2:\n");
+        scanf("%f",&num2);
+
+
+        switch (choice)
+        {
+        case 1:
+            result = add(num1 , num2);
+            printf("result : %.2f\n",result);
+            break;
+        
+        case 2:
+            result = sub(num1 , num2);
+            printf("result : %.2f\n",result);
+            break;
+        
+        case 3:
+            result = mul(num1 , num2);
+            printf("result : %.2f\n",result);
+            break;
+
+        case 4:
+            if(num2 == 0){
+                printf("Invalid");
+            }
+
+            else{
+                result = div(num1 , num2);
+                printf("result : %.2f\n",result);
+
+            }
+            
+            break;
+        
+        }
+    }
+
+    else{
+        printf("Error");
+    }
+    
+    return 0;
+}
+
+float add( float num1 , float num2 ){
+    return num1 + num2;
+}
+
+float sub( float num1 , float num2 ){
+    return num1 - num2;
+
+}
+
+float mul( float num1 , float num2 ){
+    return num1 * num2;
+}
+
+float div( float num1 , float num2 ){
+    return num1 / num2;
+}
